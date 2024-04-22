d1f5078d95a997127cdfa12144cd5b371bb45227
Jessica Slusark <slusark.jessica@gmail.com>
2024-04-22 23:48:56 +0200

    added other str functions

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,12 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strrchr.c                                          :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jessica <jessica@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:49:50 by jslusark          #+#    #+#             */
-/*   Updated: 2024/04/19 15:52:22 by jslusark         ###   ########.fr       */
+/*   Updated: 2024/04/22 23:47:01 by jessica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char *ft_strrchr(const char *s, int c)
+{
+    int i;
+    const char *ini;
+
+    ini = s;
+    i = ft_strlen(s);
+    s = (s + i);
+    while (*s != *ini && c != *s)
+        s--;
+    if (c == *s)
+        return ((char *)s);
+    return (0);
+}
