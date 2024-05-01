bf6c236602f0ac6fde32e6221707f77d3da133f4
jslusark <slusarkjessica@gmail.com>
2024-05-01 18:08:54 +0200

    fixed atoi and added memset exp

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jjs <jjs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:47:49 by jslusark          #+#    #+#             */
-/*   Updated: 2024/04/30 17:17:52 by jslusark         ###   ########.fr       */
+/*   Updated: 2024/05/01 16:49:36 by jjs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,25 @@
 
 int	ft_atoi(const char *str)
 {
-	int	num;
-	int	sign;
-	int	sign_appearance;
+	int		num;
+	int		sign;
+	size_t	i;
 
 	num = 0;
 	sign = 1;
-	sign_appearance = 0;
-	while ((*str >= 9 && *str <= 13) || *str == ' ')
-		str++;
-	while (*str == '-' || *str == '+' )
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+' )
 	{
-		if (*str == '-')
+		if (str[i] == '-')
 			sign = -1;
-		str++;
-		sign_appearance++;
+		i++;
 	}
-	if (sign_appearance > 1)
-		return (0);
-	while (ft_isdigit(*str))
+	while (ft_isdigit(str[i]))
 	{
-		num = (num * 10) + (*str - '0');
-		str++;
+		num = (num * 10) + (str[i] - '0');
+		i++;
 	}
 	return (num * sign);
 }
@@ -53,4 +50,10 @@ int	main(void)
 	printf("%d\n", atoi(" -9")); //
 	printf("%d\n", ft_atoi("-- 789")); //
 	printf("%d\n", atoi("-- 789")); //
+
+	// notes : i used size_t for i because it's usually used for indexing and loops,
+	// it's an unsigned type also, represents the size of any object which might
+	// be good when handling long indexes
+	// ontinuing to use size_t can help maintain consistency if using it through the rest of the lib
+
 } //ok */
--- ft_memset.c
+++ ft_memset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jjs <jjs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:49:15 by jslusark          #+#    #+#             */
-/*   Updated: 2024/04/30 18:05:54 by jslusark         ###   ########.fr       */
+/*   Updated: 2024/05/01 18:08:01 by jjs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,48 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*new_s;
 
+	new_s = s;
 	i = 0;
 	while (i < n)
 	{
-		((unsigned char *)s)[i] = c;
+		new_s[i] = (unsigned char)c;
 		i++;
 	}
-	return (s);
+	return (new_s);
 }
 /* #include <stdio.h>
 int main(void)
 {
 	char	string[10] = "aaaaaaaaaa";
-	printf("%s", (char*)ft_memset(string, 69, 7));
-	// printf("%s", (char*)memset(string, 69, 7));
+	printf("%s\n", (char*)ft_memset(string, 38, 7));
+	printf("%s\n", (char*)ft_memset(string, 38, 7));
+
+	printf("%s\n", (char*)ft_memset(string, -38, 7));
+	printf("%s\n", (char*)ft_memset(string, -38, 7));
+
+	int c = 511; // Example value
+    printf("Original: %d, Cast to unsigned char: %u\n", c, (unsigned char)c);
+    printf("Original: %d, Cast to char: %u\n", c, (char)c);
+    printf("Original: %d, Cast without: %u\n", c, c);
+
+	c = -38; // Example value
+    printf("Original: %d, Cast to unsigned char: %u\n", c, (unsigned char)c);
+    printf("Original: %d, Cast to char: %u\n", c, (char)c);
+    printf("Original: %d, Cast without: %u\n", c, c);
+
+	// fills a spec n of block of memory with 1 byte value (8 bit)
+	// (therefore why we use char*)
+	// decided to use unsigned* instead of *char because it's a safer way
+	// to handle memory operations and avoid that values higner
+	// than 255 or -n be misinterpreted and overflow
+	// making sure that the char byte range is between 0 to 255
+	// prevents portability issues range of possible byte values
+	// it can represent. Since a byte consists of 8 bits,
+	// it can hold values from 0 to 255 (2^8 - 1), inclusive.
+	// Each value within this range corresponds to a unique binary
+	// pattern of 8 bits,ranging from all 0s (representing 0)
+	// to all 1s (representing 255).
 } */
