1b813a5612fce771437afe6ed1542fdc5d27a3ec
jslusark <slusarkjessica@gmail.com>
2024-05-02 09:43:31 +0200

    changed str names

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,32 +6,32 @@
 /*   By: jjs <jjs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:49:32 by jslusark          #+#    #+#             */
-/*   Updated: 2024/05/01 21:20:56 by jjs              ###   ########.fr       */
+/*   Updated: 2024/05/02 09:43:20 by jjs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	h;
 	size_t	n;
 
 	h = 0;
-	if (little[h] == '\0')
-		return ((char *)big);
-	while (big[h] != '\0')
+	if (needle[h] == '\0')
+		return ((char *)haystack);
+	while (haystack[h] != '\0')
 	{
 		n = 0;
-		while (big[h] == little[n] && (h + n) < len)
+		while (haystack[h] == needle[n] && (h + n) < len)
 		{
-			if (big[h] == '\0' && little[n] == '\0')
-				return ((char *)&big[h]);
+			if (haystack[h] == '\0' && needle[n] == '\0')
+				return ((char *)&haystack[h]);
 			n++;
 			h++;
 		}
-		if (little[n] == '\0')
-			return ((char *)big + h);
+		if (needle[n] == '\0')
+			return ((char *)haystack + h);
 		h++;
 	}
 	return (0);
