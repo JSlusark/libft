346d697abcdc1a2e4e4046a89c56379388c2bd3e
jslusark <slusarkjessica@gmail.com>
2024-05-02 09:31:42 +0200

    fixed memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jjs <jjs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:48:37 by jslusark          #+#    #+#             */
-/*   Updated: 2024/05/01 21:23:49 by jjs              ###   ########.fr       */
+/*   Updated: 2024/05/02 09:28:52 by jjs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,23 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*str1;
-	unsigned char	*str2;
 	size_t			i;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
 	i = 0;
 	if (n == 0)
 		return (0);
 	while (i < n)
 	{
-		if (*str1 != *str2)
-			return (*str1 - *str2);
-		str1++;
-		str2++;
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
 	}
-
 	return (0);
 }
+/* int	main(void)
+{
+	char	str1;
+	char	str2;
+
+	printf(memcmp());
+} */
